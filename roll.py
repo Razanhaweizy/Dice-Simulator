a25cb90717e0e79be47da30f7e44dd14ac35ded1
Razanhaweizy <Razanrh05@gmail.com>
2026-01-04 20:42:10 +0300

    added docstring to functions and removed unneccassry param

--- roll.py
+++ roll.py
@@ -6,8 +6,10 @@ terminate = False
 dice_counter = 0
 die_face = 6 #default is 6
 
+#dict that tracks the number of times a die digit is rolled
 num_count = {}
 
+#dict that tracks certain stats of all dies rolled
 stats_dict = {
     "sum_rolls": 0,
     "average": 0.0,
@@ -15,15 +17,24 @@ stats_dict = {
     "least_common": 0,
 }
 
+#a list that tracks all the valid faces that this die can have
 valid_faces = ["d4", "d6", "d8", "d10", "d12", "d20", "d100"]
 
-def populate_dict():
+def populate_dict() -> None:
+    '''Helper function that populates the num_count dictionary with default (0) values
+    depending on the face of the die .
+    Example: d10 die will have keys 1-10 with value 0.'''
     num_count.clear()
     for i in range(1, die_face + 1):
         num_count[i] = 0
     return
 
 def reset_stats(user_reset=False) -> None:
+    '''Helper function that resets all stats (dice_counter, num_count, and stats_dict)
+    to their default values which are 0.
+    Param user_reset: default false. If true, means that user requested reset so the die
+    face will also be reset. If false, used in changing die face to reset all values except
+    die face.'''
     #only reset die face if user manually reset stats
     global die_face
     if user_reset:
@@ -41,8 +52,8 @@ def reset_stats(user_reset=False) -> None:
     populate_dict()
     return
 
-#TODO add docstring
 def display_menu() -> None:
+    '''Helper function that prints out all menu options (features) to the terminal'''
     print("* Type 'y' to roll a die")
     print("* Type 'n' to terminate the program")
     print("* Type 'c' to view how many dice you have rolled")
@@ -52,8 +63,10 @@ def display_menu() -> None:
     print("* Type 'r' to reset your stats. This will also reset your chosen die face to default")
     return
 
-#TODO add docstring
 def get_die_face(choice: str) -> None:
+    '''Helper function that updates the die face to the user's choosing and
+    resets other stats
+    Param choice: the user's input which represents what die face they want'''
     if not choice in valid_faces:
         print("Invalid die face choice @_@ ")
         return
@@ -67,8 +80,10 @@ def get_die_face(choice: str) -> None:
     populate_dict()
     return
 
-#TODO add docstring
-def get_valid_int(prompt: str) -> int:
+def get_valid_int() -> int:
+    '''Helper function that asks and parses the user's option for how many die to roll
+    and validates the input
+    Catches incorrect input and asks user to re-enter in that case'''
     while True:
         dices = input("How many dice to roll? ")
         try:
@@ -77,9 +92,8 @@ def get_valid_int(prompt: str) -> int:
         except ValueError:
             print("Invalid number, please enter a valid integer -_-")
            
-#TODO add docstring
-#once face is changed, maybe clear stats?
-def update_stats(roll: int):
+def update_stats(roll: int) -> None:
+    '''Helper function that updates the tracked stats after every die roll'''
     num_count[roll] += 1 
     stats_dict["sum_rolls"] += roll
     stats_dict["average"] = stats_dict["sum_rolls"] / dice_counter
@@ -87,16 +101,21 @@ def update_stats(roll: int):
     min_val = min(num_count.values())
     stats_dict["most_common"] = [key for key, value in num_count.items() if value == max_val]
     stats_dict["least_common"] = [key for key, value in num_count.items() if value == min_val]
+    return
 
-#TODO add docstring
-def display_stats():
+def display_stats() -> None:
+    '''Helper function that prints to terminal the current stats'''
     print(f"The sum of all rolls is: {stats_dict['sum_rolls']}")
     print(f"The average across all rolls is: {stats_dict['average']}")
     print(f"The most common value(s) rolled is: {stats_dict['most_common']}")
     print(f"The least common value(s) rolled is: {stats_dict['least_common']}")
+    return
 
-#TODO add docstring
 def draw_histogram(data, bar_char='*') -> None:
+    '''Helper function that draws a histogram that shows frequency of each number
+    rolled
+    Param data: List of tuples where each tuple represents value and its frequency
+    Param bar_char: Char used to represent frequency, optional. Default value: * '''
     freq_lst = []
     for tup in data:
         for i in range(tup[1]):
@@ -113,17 +132,20 @@ def draw_histogram(data, bar_char='*') -> None:
     for label, count in sorted(counts.items()):
         bar = bar_char * count
         print(f"{str(label).rjust(max_label_length)} | {bar} ")
+
+    return
     
 
-populate_dict()
+populate_dict() #populate dict before starting
 
+#main program loop
 while not terminate:
     if dice_counter == 100:
         print("Wow, you're locked in... you have officially rolled 100 times")
 
     user_input = input("Roll the dice? Type m to view the menu :3 ").lower()
     if user_input == "y":
-        dices = get_valid_int("How many dice to roll? ")
+        dices = get_valid_int()
         rolls = []
         for i in range(dices):
             dice_counter += 1 #track num of dice rolls
